84387ebdbde35c7203abfc23327de6c9a3477906
Apprentice <apprentice@dbc-23.local>
2013-10-28 20:46:52 -0700

    attempt at incorporating partials

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 DannieCraigslist::Application.routes.draw do
   root to: 'categories#index'
-  # get '/categories/:id', to: 'posts#index'
+
   resources :categories, only: [:index] do
     resources :posts
   end
